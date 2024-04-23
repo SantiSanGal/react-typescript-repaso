aa1ba815a2fcbb9d3bb9b86089aa07557be887ab
SantiSanGal <santiagon.g.m@gmail.com>
2024-04-23 12:08:48 -0400

    primer commit

---
+++ src/components/Usuario.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+interface User {
+    uid: string;
+    name: string;
+}
+
+const tempUser: User = {
+    uid: 'xyz890',
+    name: 'Default Name'
+}
+
+export const Usuario = () => {
+    const [user, setUser] = useState<User>(tempUser);
+
+    const login = () => {
+        setUser({
+            uid: 'ABC123',
+            name: 'Santiago Galván'
+        })
+    }
+
+    return (
+        <div className="mt-5">
+            <h3> Usuario: useState</h3>
+
+            <button
+                onClick={login}
+                className="btn btn-outline-primary">
+                Login
+            </button>
+
+            {
+                (!user)
+                    ? <pre>No Hay Usuario</pre>
+                    : <pre> {JSON.stringify(user)} </pre>
+            }
+
+        </div>
+    )
+}
---
+++ src/react-app-env.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react-scripts" />
+
+/*En typescript los archivos que tienen la extensión .d.ts son
+archivos de definición */
